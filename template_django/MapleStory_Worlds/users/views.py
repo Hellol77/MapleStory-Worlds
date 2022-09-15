3c325515594af140ac7d7b8e8146b5b1ff6c7d09
wJJin <wjin9707@hufs.ac.kr>
2022-09-16 00:32:18 +0900

    Bug: index.html error

--- template_django/MapleStory_Worlds/users/views.py
+++ template_django/MapleStory_Worlds/users/views.py
@@ -33,17 +33,54 @@ def main_view(request):
             link5="None"
     else:
         link1, link2, link3, link4, link5 = "None", "None", "None", "None", "None"
+
+    mission1 = Mission_1.objects.order_by('-pk')[:4]
+    for i in range(len(mission1)):
+        if mission1[i].image1 == '':
+            mission1[i].image1 = "/static/board/img/nothumnail.png"
+        else:
+            mission1[i].image1 = "/media/"+str(mission1[i].image1)
+
+    mission2 = Mission_2.objects.order_by('-pk')[:4]
+    for i in range(len(mission2)):
+        if mission2[i].image1 == '':
+            mission2[i].image1 = "/static/board/img/nothumnail.png"
+        else:
+            mission2[i].image1 = "/media/"+str(mission2[i].image1)
+
+    mission3 = Mission_3.objects.order_by('-pk')[:4]
+    for i in range(len(mission3)):
+        if mission3[i].image1 == '':
+            mission3[i].image1 = "/static/board/img/nothumnail.png"
+        else:
+            mission3[i].image1 = "/media/"+str(mission3[i].image1)
+
+    mission4 = Mission_4.objects.order_by('-pk')[:4]
+    for i in range(len(mission4)):
+        if mission4[i].image1 == '':
+            mission4[i].image1 = "/static/board/img/nothumnail.png"
+        else:
+            mission4[i].image1 = "/media/"+str(mission4[i].image1)
+
+    mission5 = Mission_5.objects.order_by('-pk')[:4]
+    for i in range(len(mission5)):
+        if mission5[i].image1 == '':
+            mission5[i].image1 = "/static/board/img/nothumnail.png"
+        else:
+            mission5[i].image1 = "/media/"+str(mission5[i].image1)
+
     context  = {
         "link1":link1,
         "link2":link2,
         "link3":link3,
         "link4":link4,
         "link5":link5,
+        "mission1":mission1,
+        "mission2":mission2,
+        "mission3":mission3,
+        "mission4":mission4,
+        "mission5":mission5,
     }
-    a = "abcd"
-    hashed_pass = make_password(a)
-    print(hashed_pass)
-
     return render(request, 'users/index.html', context)
 
 class LoginView(FormView):
@@ -63,7 +100,7 @@ class LoginView(FormView):
         return super().form_valid(form)
 
     def form_invalid(self, form: LoginForm):
-        messages.error(self.request, '로그인에 실패하였습니다.', extra_tags='danger')
+        messages.error(self.request, '아이디 비밀번호를 확인해주세요.')
         return super().form_invalid(form)
 
 
